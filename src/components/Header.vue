13de800ce3a0cd843096d689099eb6a7b828edec
viktoralyoshin <viktor.aleshin2004@gmail.com>
2023-08-19 02:20:26 +0800

    changed colors

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,7 +4,7 @@
   <div class="h-screen flex max-w-[1200px] mx-auto items-center">
     <div class="flex flex-col">
       <div class="text-white text-[26px] w-[600px] flex flex-col gap-5">
-        <h1 class="text-[48px] text-purple-500  font-medium">&ltFuture - Web-Studio/&gt</h1>
+        <h1 class="text-[46px] text-green-500  font-medium font-head">&ltWebLab - Web-Studio/&gt</h1>
         <h1>Разработаем сайт, который привлечёт, удержит и превратит посетителей в клиентов!</h1>
       </div>
     </div>
--- src/components/Header/Navbar.vue
+++ src/components/Header/Navbar.vue
@@ -3,7 +3,7 @@
     <div class="max-w-[1200px] items-center mx-auto">
     <div class="flex justify-between text-white my-6 mx-4">
       <div class="flex items-center">
-        <h1 class="font-[500] text-[24px] font-head tracking-wider">Future</h1>
+        <h1 class="font-[500] text-[24px] font-head tracking-wider"><span class="text-green-500">Web</span>Lab</h1>
       </div>
       <ul class="text-white font-medium flex items-center gap-6 text-[17px]">
         <li class="opacity-70 hover:opacity-100 duration-200 ease-in">Главная</li>
@@ -12,7 +12,7 @@
         <li class="opacity-70 hover:opacity-100 duration-200 ease-in">О нас</li>
       </ul>
       <a
-        class="decoration-none no-underline bg-purple-900 hover:bg-purple-700 rounded-md text-white font-medium duration-200 ease-in hover:shadow-sm"
+        class="decoration-none no-underline bg-green-800 hover:bg-green-500 rounded-md text-white font-medium duration-200 ease-in hover:shadow-sm"
         href="https://vk.com/im?sel=-222064767"
         ><div class="mx-6 my-2">Написать ВКонтакте</div></a
       >
